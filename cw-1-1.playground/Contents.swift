1a4b988907c6a2c945c6bc84ab2fbc53341f9980
ald278 <dealdaihane1@gmail.com>
2020-09-09 20:55:28 +0300

    done

---
+++ cw-1-1.playground/Contents.swift
@@ -0,0 +1,13 @@
+let name = " deemah saad aldaihane "
+var hight = 1.8
+var age = 17
+var hobby1 = " reading "
+var hobby2 = " swimming "
+print(" السلام عليكم اسمي \(name)انا احب \(hobby1) \(hobby2) عمري \(age) طولي \(hight) ")
+let friendName = " aldana saad alrshide "
+var aldanaHight = 1.7
+var aldanaAge = 17
+var aldanaHobby1 = " swimming "
+var aldanaHobby2 = " reading "
+var ageDifferent = 17-17
+var mutualHobby = (hobby1 == aldanaHobby1) || (hobby1 == aldanaHobby2)
